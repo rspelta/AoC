1911ba08e79bcefc407d94997d6781277e8ad723
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-03 22:24:13 +0100

    delete target

--- day-02/target/debug/deps/day_02-73f55bde222b1f19.d
+++
@@ -1,6 +0,0 @@
-/tmp/AoC/day-02/target/debug/deps/libday_02-73f55bde222b1f19.rmeta: src/main.rs src/./input.txt
-
-/tmp/AoC/day-02/target/debug/deps/day_02-73f55bde222b1f19.d: src/main.rs src/./input.txt
-
-src/main.rs:
-src/./input.txt:
--- day-02/target/debug/deps/day_02-a0c6a842b61ff0ce.d
+++
@@ -1,6 +0,0 @@
-/tmp/AoC/day-02/target/debug/deps/libday_02-a0c6a842b61ff0ce.rmeta: src/main.rs src/./input.txt
-
-/tmp/AoC/day-02/target/debug/deps/day_02-a0c6a842b61ff0ce.d: src/main.rs src/./input.txt
-
-src/main.rs:
-src/./input.txt:
