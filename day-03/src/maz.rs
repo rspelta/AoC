544e03cb9d4006223b6db303a0cfac9203269985
Maaz Ullah <mullah@tecniplast.it>
2024-12-03 22:32:15 +0100

    maz part2

--- day-03/src/maz.rs
+++ day-03/src/maz.rs
@@ -5,18 +5,27 @@ fn main() {
 }
 
 fn part1_maz() {
-    let re: Regex = Regex::new(r"mul\(\d+,\d+\)").unwrap();
+    let re: Regex = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)").unwrap();
     let file_txt = include_str!("input.txt");
 
     let mut res : i32 = 0;
+    let mut compute: bool = true;
 
     for val in re.find_iter(&file_txt) {
-        let vals: Vec<&str> = val.as_str().trim_start_matches("mul(").trim_end_matches(")").split(",").collect();
-        
-        let x: i32 = vals[0].parse().unwrap();
-        let y: i32 = vals[1].parse().unwrap();
-
-        res += x*y
+        match val.as_str() {
+            "do()" => {compute = true; continue},
+            "don't()" => {compute = false; continue;},
+            &_ => {
+                if compute {
+                    let vals: Vec<&str> = val.as_str().trim_start_matches("mul(").trim_end_matches(")").split(",").collect();
+            
+                    let x: i32 = vals[0].parse().unwrap();
+                    let y: i32 = vals[1].parse().unwrap();
+            
+                    res += x*y
+                }
+            },
+        }
     }
 
     println!("{}", res)
