b1d2902d62b803ae57acc9aad618e0c4ba374496
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-03 22:20:44 +0100

    day 3-2

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -18,7 +18,41 @@ fn part1() -> i32 {
     sum
 }
 
-fn main() {
+fn part2() -> i32 {
+    let re: Regex = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)").unwrap();
+    let file_txt = include_str!("input.txt");
 
+    let mut res : i32 = 0;
+    let mut _do = true;
+
+    for val in re.find_iter(&file_txt) {
+        let matched = val.as_str();
+
+        match matched {
+            s if s.starts_with("mul") => {
+                if _do {
+                    let vals: Vec<&str> = val.as_str().trim_start_matches("mul(").trim_end_matches(")").split(",").collect();
+            
+                    let x: i32 = vals[0].parse().unwrap();
+                    let y: i32 = vals[1].parse().unwrap();
+            
+                    res += x*y
+                }
+            }
+            "do()" => {
+                _do = true
+            }
+            "don't()" => {
+                _do = false
+            }
+            _ => {}
+        }
+    }
+
+    res
+}
+
+fn main() {
     println!("{}", part1());
+    println!("{}", part2());
 }
