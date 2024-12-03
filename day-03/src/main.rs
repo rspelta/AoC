10b674ff416c9d94804702fd6a9c86da8d6fc32f
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-03 21:59:23 +0100

    day3-1

--- day-03/src/main.rs
+++ day-03/src/main.rs
@@ -1,3 +1,24 @@
+use regex::Regex;
+
+fn part1() -> i32 {
+    let text = include_str!("./input.txt");
+    let mut sum : i32 = 0;
+
+    let re_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    // Itera su tutte le corrispondenze della regex
+    for captures in re_mul.captures_iter(text) {
+        // Estrai i numeri come stringhe e convertili in i32
+        if let (Some(first), Some(second)) = (captures.get(1), captures.get(2)) {
+            let first_number = first.as_str().parse::<i32>().unwrap();
+            let second_number = second.as_str().parse::<i32>().unwrap();
+            sum += first_number * second_number;
+        }
+    }
+    sum
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("{}", part1());
 }
