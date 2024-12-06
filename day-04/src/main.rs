8b92575d54a7cd09bfa5c0e9fabaa6457996dff0
Maaz Ullah <mullah@tecniplast.it>
2024-12-06 22:42:18 +0100

    check functions

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -12,3 +12,18 @@ fn main() {
     println!("{}", part1());
     println!("{}", part2());
 }
+
+// working
+fn isXmasHorizontal(x i32, y i32, input Vec<Vec<i32>>) -> bool {
+    return (input[y][x] == 'X' && input[y][x+1] == 'M' && input[y][x+2] == 'A' && input[y][x+1] == 'S') || (input[y][x] == 'S' && input[y][x+1] == 'A' && input[y][x+2] == 'M' && input[y][x+1] == 'X')
+}
+
+// not working
+fn isXmasVertical(x i32, y i32, input Vec<Vec<i32>>) -> bool {
+    return (input[y][x] == 'X' && input[y+1][x] == 'M' && input[y+2][x] == 'A' && input[y+1][x] == 'S') || (input[y][x] == 'S' && input[y+1][x] == 'A' && input[y+1][x+2] == 'M' && input[y+1][x] == 'X')
+}
+
+// not working
+fn isXmasDiagonal(x i32, y i32, input Vec<Vec<i32>>) -> bool {
+    return (input[y][x] == 'X' && input[y+1][x+1] == 'M' && input[y+1][x+1] == 'A' && input[y+1][x] == 'S') || (input[y][x] == 'S' && input[y+1][x] == 'A' && input[y+1][x+2] == 'M' && input[y+1][x] == 'X')
+}
