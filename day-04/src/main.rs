32336e2fb39acac986389dde33a1023f2a054cce
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-06 22:06:33 +0100

    day04

---
+++ day-04/src/main.rs
@@ -0,0 +1,14 @@
+use regex::Regex;
+
+fn part1() -> i32 {
+    let text = include_str!("./input.txt");
+    let mut sum : i32 = 0;
+
+    sum
+}
+
+
+fn main() {
+    println!("{}", part1());
+    println!("{}", part2());
+}
