6eeaa2d98e0e34b1d090593988b2bd1672a4aee4
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-07 18:11:53 +0100

    done

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -7,8 +7,8 @@ fn extract_numbers(text: &str) -> HashMap<i32, Vec<i32>> {
     {
         if let Some((parte1, parte2)) = line.split_once("|") {
             // Converte entrambe le parti in i32
-            let key: i32 = parte1.parse().expect("Conversione a i32 fallita");
-            let value: i32 = parte2.parse().expect("Conversione a i32 fallita");
+            let key: i32 = parte1.parse().unwrap();
+            let value: i32 = parte2.parse().unwrap();
     
             dizionario.entry(key).or_insert(Vec::new()).push(value);
         } else {
@@ -19,7 +19,7 @@ fn extract_numbers(text: &str) -> HashMap<i32, Vec<i32>> {
     dizionario
 }
 
-fn get_line_pages( text: &str ) -> i32
+fn get_line_pages( text: &str ) -> usize
 {
     let mut line_count = 0;
 
@@ -32,6 +32,15 @@ fn get_line_pages( text: &str ) -> i32
     0
 }
 
+fn are_pages_valid(pages: &[i32], checklist: &Vec<i32>) -> bool {
+    for page in pages {
+        if checklist.contains(page) {
+            return false;
+        }
+    }
+    true
+}
+
 fn part1() -> i32 {
     let text = include_str!("./input.txt");
     let mut sum : i32 = 0;
@@ -40,11 +49,31 @@ fn part1() -> i32 {
 
     let start_line_pages = get_line_pages(text);
 
+    for line in text.lines().skip(start_line_pages) 
+    {
+        let mut is_valid = true;
+        let pages: Vec<i32> = line.split(',').filter_map(|s| s.parse().ok()).collect();
 
+
+        for (i, v) in pages.iter().enumerate().rev() { 
+            if dizionario.contains_key(&v) {
+                let checklist = dizionario.get(&v).unwrap();
+                if !are_pages_valid( &pages[0..i], checklist ) {
+                    is_valid = false;
+                    break;
+                }
+            }
+        }
+        if is_valid {
+            sum += pages[pages.len()/2]
+        }
+    }
+
+/*
     for (key, value) in dizionario {
         println!("key: {}, value: {:?} {}", key, value, start_line_pages);
         sum+=1;
-    }
+    }*/
 
     sum
 }
