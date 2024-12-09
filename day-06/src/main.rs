6eab9406bb7ad5640d5b9f3faa7728d38a3cdb89
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-09 20:51:29 +0100

    read map

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,3 +1,33 @@
+
+
+fn part1() -> i32 {
+    let text = include_str!("./input.txt");
+    let mut sum : i32 = 0;
+    #[derive(Debug)]
+    struct Location(i32, i32, char);
+
+    let mut map = Vec::new();
+    let mut start: Location = Location(0, 0, '#');
+
+    for (y, line) in text.lines().enumerate() {
+        for (x, cell) in line.chars().enumerate() {
+            match cell {
+                '#' => {
+                    map.push(Location(x as i32, y as i32, '#'));
+                }
+                '^' => {
+                    start = Location(x as i32, y as i32, '^')
+                }
+                _ => {}
+
+            }
+        }
+    }
+    println!("map size: {:#?}, start: {:?}", map, start);
+    sum
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", part1());
+    //println!("{}", part2());
 }
