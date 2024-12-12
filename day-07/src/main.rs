6ec7b194eb6e1ce77b297ca0e4ffef755fe430c8
Roberto Spelta <roberto.spelta@tecniplast.it>
2024-12-12 14:14:09 +0100

    done

--- day-07/src/main.rs
+++ day-07/src/main.rs
@@ -1,32 +1,53 @@
-use itertools::Itertools;
 
-fn calculate_combinations(nums: &[i32], operators: &[char]) -> Vec<String> {
-    let mut results = Vec::new();
-
-    // Generare tutte le combinazioni di operatori
-    let operator_combinations = (0..=operators.len())
-        .map(|_| operators)
-        .multi_cartesian_product();
-
-    for combination in operator_combinations {
-        let mut expression = nums[0].to_string(); // Inizia dal primo numero
-        for (num, op) in nums.iter().skip(1).zip(&combination) {
-            expression.push(**op); // Aggiungi l'operatore
-            expression.push_str(&num.to_string()); // Aggiungi il numero
-        }
-        results.push(expression);
-    }
-
-    results
+fn get_union(a: u64, b: u64) -> u64 {
+    format!("{}{}", a, b).parse::<u64>().unwrap()
 }
 
-fn main() 
-{
-    let nums = vec![9, 7, 18, 13];
-    let operators = vec!['+', '*'];
-
-    let combinations = calculate_combinations(&nums, &operators);
-    for comb in &combinations {
-        println!("{}", comb);
+ fn foo( now: u64, mut values: Vec<u64>, result: u64, counter: &mut u64) -> ()
+ {
+    if values.len() == 0 {
+        //println!("res: {}", now);
+        if now == result {
+            *counter += 1;
+        }
+        return ();
     }
+
+    let val = values.pop().unwrap();
+    //println!("{} * {} = {}", now, val, now * val);
+    foo( now * val, values.clone(), result, counter);
+
+    foo( get_union(now, val), values.clone(), result, counter);
+
+    //println!("{} + {} = {}", now, val, now + val);
+    foo( now + val, values.clone(), result, counter);
+ }
+
+
+fn part1() -> u64 {
+    let mut sum = 0;
+    let mut counter: u64;
+    let input = include_str!("input.txt");
+
+    for line in input.lines() {
+        counter = 0;
+        let mut values = line.split(':').nth(1).unwrap().split_whitespace().map(|x| x.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+        let result = line.split(':').nth(0).unwrap().parse::<u64>().unwrap();
+        values.reverse();
+        //println!("result: {} values: {:?}", result, values);
+        let val = values.pop().unwrap();
+
+        foo( val, values, result, &mut counter);
+
+        //println!("counter: {}", counter);
+        if counter > 0 {
+            sum += result;   
+        }
+    }
+    sum
+}
+
+fn main() {
+    println!("{}", part1());
+    //println!("{}", part2());
 }
